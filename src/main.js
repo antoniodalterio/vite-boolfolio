809e42abb7119741d3f3496c9df6056cf9134ec4
antoniodalterio <antoniodalterio2227@gmail.com>
2024-02-15 18:04:46 +0100

    feat: scaffolding vite

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import './scss/style.scss';
+
+// Import our custom CSS
+import './scss/style.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import App from './App.vue';
+
+createApp(App).mount('#app');
